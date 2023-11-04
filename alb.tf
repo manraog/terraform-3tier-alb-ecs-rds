a0067660f6ea7720b6a66b15c9f3af9deec15883
Ricardo Ortega <ricardo.raog@gmail.com>
2023-11-04 15:56:32 -0600

    feat(alb): added application load balancer with tls

---
+++ alb.tf
@@ -0,0 +1,45 @@
+# create application load balancer
+resource "aws_lb" "application_load_balancer" {
+  name                       = "alb-${var.project}-${var.environment}"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.alb_security_group.id]
+  subnets                    = [aws_subnet.public_subnet_az1.id, aws_subnet.public_subnet_az2.id]
+  enable_deletion_protection = false
+}
+
+# TODO: add https connection between alb and ecs
+resource "aws_lb_target_group" "alb_target_group" {
+  name        = "tg-${var.project}-${var.environment}"
+  target_type = "ip"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.vpc.id
+
+  health_check {
+    healthy_threshold   = 5
+    interval            = 30
+    matcher             = "200,301,302"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+}
+
+#TODO: add htt to https redirection
+
+# create a listener on port 443 with forward action
+resource "aws_lb_listener" "alb_https_listener" {
+  load_balancer_arn  = aws_lb.application_load_balancer.arn
+  port               = 443
+  protocol           = "HTTPS"
+  ssl_policy         = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn    = aws_acm_certificate.acm_certificate.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
