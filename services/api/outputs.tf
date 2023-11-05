dd5d3528fd51deeb7acae5490ada918b99add467
Ricardo Ortega <ricardo.raog@gmail.com>
2023-11-05 00:13:06 -0600

    fix(services/api): non sensitive value

--- services/api/outputs.tf
+++ services/api/outputs.tf
@@ -1,5 +1,5 @@
 output service_url {
-  value       = "https://${var.api_service_name}.${data.tfe_outputs.infra.values.r53_base_domain}"
+  value       = nonsensitive("https://${var.api_service_name}.${data.tfe_outputs.infra.values.r53_base_domain}")
   sensitive   = false
   description = "URL to consume the service"
 }
