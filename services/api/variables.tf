f0fa81c9a02c2913f9302ea22e0afbcbb0706d2f
Ricardo Ortega <ricardo.raog@gmail.com>
2023-11-04 23:09:56 -0600

    fix(infra): renamed sg

--- services/api/variables.tf
+++ services/api/variables.tf
@@ -45,7 +45,7 @@ variable api_service_name {
 
 variable api_image_port {
   type        = number
-  default     = 8080
+  default     = 80
   description = "Port where the application listens"
 }
 
